52c27001574f9d437380eb20c364f046cc72997d
Matt Hamil <j.matthew.hamil@gmail.com>
2016-08-31 14:55:12 -0500

    Can add new tasks

--- app/app.js
+++ app/app.js
@@ -1,34 +1,66 @@
 "use strict";
 
-$scope.items = [
-  {
-    id: 0,
-    task: "mow the lawn",
-    isCompleted: false,
-    dueDate: "12/5/17",
-    assignedTo: "Greg",
-    location: "Joe's house",
-    urgency: "low",
-    dependencies: "sunshine, clippers, hat, water, headphones"
-  },
-  {
-    id: 1,
-    task: "grade quizzes",
-    isCompleted: false,
-    dueDate: "12/5/15",
-    assignedTo: "Christina",
-    location: "NSS",
-    urgency: "high",
-    dependencies: "wifi, tissues, vodka"
-  },
-  {
-    id: 2,
-    task: "take a nap",
-    isCompleted: false,
-    dueDate: "5/21/16",
-    assignedTo: "Joe",
-    location: "Porch of lakefront cabin",
-    urgency: "medium",
-    dependencies: "hammock, silence"
-  }
-];
+let app = angular.module('TodoApp', []);
+
+app.controller('NavCtrl', ($scope) => {
+  $scope.navItems = [
+    { name: "Logout" },
+    { name: "All Items" },
+    { name: "New Items" }
+  ];
+});
+
+app.controller('TodoCtrl', ($scope) => {
+  $scope.items = [
+    {
+      id: 0,
+      task: "mow the lawn",
+      isCompleted: false,
+      dueDate: "12/5/17",
+      assignedTo: "Greg",
+      location: "Joe's house",
+      urgency: "low",
+      dependencies: "sunshine, clippers, hat, water, headphones"
+    },
+    {
+      id: 1,
+      task: "grade quizzes",
+      isCompleted: false,
+      dueDate: "12/5/15",
+      assignedTo: "Christina",
+      location: "NSS",
+      urgency: "high",
+      dependencies: "wifi, tissues, vodka"
+    },
+    {
+      id: 2,
+      task: "take a nap",
+      isCompleted: false,
+      dueDate: "5/21/16",
+      assignedTo: "Joe",
+      location: "Porch of lakefront cabin",
+      urgency: "medium",
+      dependencies: "hammock, silence"
+    }
+  ];
+
+  $scope.newTask = {};
+  $scope.showListView = true;
+
+  $scope.newItem = () => {
+    $scope.showListView = false;
+  };
+
+  $scope.allItem = () => {
+    $scope.showListView = true;
+  };
+
+  $scope.addNewItem = () => {
+    $scope.newTask.isCompleted = false;
+    $scope.newTask.id = $scope.items.length;
+    $scope.items.push($scope.newTask);
+    $scope.newTask = {};
+  };
+});
+
+
