5627fe8d5e7bd5f98f35502b7e04891d9a7b2322
Matt Hamil <j.matthew.hamil@gmail.com>
2016-09-01 11:07:28 -0500

    WIP

--- app/controllers/NavCtrl.js
+++ app/controllers/NavCtrl.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.controller('NavCtrl', function($scope, SearchTermData) {
+app.controller('NavCtrl', function($scope, SearchTermData, $location) {
   $scope.searchText = SearchTermData;
 
   $scope.navItems = [
@@ -8,4 +8,6 @@ app.controller('NavCtrl', function($scope, SearchTermData) {
     { name: 'All Items', url: '#/items/list' },
     { name: 'New Items', url: '#/items/new' }
   ];
+
+  $scope.isActive = (viewLocation) => viewLocation === $location.path();
 });
